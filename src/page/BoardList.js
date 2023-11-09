af37f45f677764b9c337bf7569822f5008244ea5
hrhrhrasd <gmleh1245@gmail.com>
2023-11-09 16:18:22 +0900

    Spinner 를 테이블 밖으로 뺌

--- src/page/BoardList.js
+++ src/page/BoardList.js
@@ -15,11 +15,14 @@ import { useNavigate } from "react-router-dom";
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
 
+  let navigate = useNavigate();
+
   useEffect(() => {
     axios.get("/api/board/list").then(({ data }) => setBoardList(data));
   }, []);
-
-  let navigate = useNavigate();
+  if (boardList === null) {
+    return <Spinner />;
+  }
 
   return (
     <Box>
@@ -35,24 +38,20 @@ export function BoardList() {
             </Tr>
           </Thead>
           <Tbody>
-            {boardList === null ? (
-              <Spinner />
-            ) : (
-              boardList.map((board) => (
-                <Tr
-                  _hover={{
-                    cursor: "pointer",
-                  }}
-                  key={board.id}
-                  onClick={() => navigate("/board/" + board.id)}
-                >
-                  <Td>{board.id}</Td>
-                  <Td>{board.title}</Td>
-                  <Td>{board.writer}</Td>
-                  <Td>{board.inserted}</Td>
-                </Tr>
-              ))
-            )}
+            {boardList.map((board) => (
+              <Tr
+                _hover={{
+                  cursor: "pointer",
+                }}
+                key={board.id}
+                onClick={() => navigate("/board/" + board.id)}
+              >
+                <Td>{board.id}</Td>
+                <Td>{board.title}</Td>
+                <Td>{board.writer}</Td>
+                <Td>{board.inserted}</Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </Box>
