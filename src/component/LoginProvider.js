9cf39b80fb84e3c0b5d2f9fc72bd9706b1adaa54
hrhrhrasd <gmleh1245@gmail.com>
2023-11-14 16:37:17 +0900

    LoginProvider에 로그인 기능 정리

---
+++ src/component/LoginProvider.js
@@ -0,0 +1,41 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const LoginContext = createContext(null);
+function LoginProvider({ children }) {
+  const [login, setLogin] = useState("");
+
+  useEffect(() => {
+    fetchLogin();
+  }, []);
+
+  function fetchLogin() {
+    axios.get("/api/member/login").then(({ data }) => setLogin(data));
+  }
+
+  function isAuthenticated() {
+    return login !== "";
+  }
+
+  function isAdmin() {
+    if (login.auth) {
+      return login.auth.some((elem) => elem.name === "admin");
+    }
+
+    return false;
+  }
+
+  function hasAccess(userId) {
+    return login.id === userId;
+  }
+
+  return (
+    <LoginContext.Provider
+      value={{ login, fetchLogin, isAuthenticated, hasAccess, isAdmin }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
+}
+
+export default LoginProvider;
