cab5b380c47c983f423435823be19b376b7aa79b
hrhrhrasd <gmleh1245@gmail.com>
2023-11-14 11:39:41 +0900

    board edit 작성자 삭제

--- src/page/board/BoardEdit.js
+++ src/page/board/BoardEdit.js
@@ -20,7 +20,6 @@ import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useImmer } from "use-immer";
 import axios from "axios";
-import { logDOM } from "@testing-library/react";
 
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null);
@@ -93,17 +92,6 @@ export function BoardEdit() {
           onChange={(e) =>
             updateBoard((draft) => {
               draft.content = e.target.value;
-            })
-          }
-        />
-      </FormControl>
-      <FormControl>
-        <FormLabel>작성자</FormLabel>
-        <Input
-          value={board.writer}
-          onChange={(e) =>
-            updateBoard((draft) => {
-              draft.writer = e.target.value;
             })
           }
         />
